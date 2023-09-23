07e9c396f1f3c0d8ab73c4ee032715101fbb0ad0
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2023-09-23 16:51:00 +0100

    Squashed commit of the following:
    commit 05d74751fc0c9b8237d217b6640851911fd34448
    Merge: 1484da26 bddbca5c
    Author: Keyrxng <106303466+Keyrxng@users.noreply.github.com>
    Date:   Thu Sep 21 19:25:49 2023 +0100
    
        Merge pull request #57 from KxyChain/main
    
        test
    
    commit 1484da26ad9d937f0f78da12e3b0c1afc21a004c
    Author: Keyrxng <106303466+Keyrxng@users.noreply.github.com>
    Date:   Wed Sep 20 21:11:39 2023 +0100
    
        Update Counter.sol
    
    commit a47e47a242f96a8a64f9924bab4c267a725705a8
    Author: Keyrxng <106303466+Keyrxng@users.noreply.github.com>
    Date:   Wed Sep 13 20:49:03 2023 +0100
    
        fix: updated test and src
    
        test fixes and counter init fix
    
    commit 7c21782fa1c4059bbed17665d78dda31323ec15b
    Author: Keyrxng <106303466+Keyrxng@users.noreply.github.com>
    Date:   Sat Sep 9 11:02:34 2023 +0100
    
        chore: rename deploy script
    
        update counter deploy script name
    
    commit afcf65c5812e6f3e9d07d65a21a26c0dbd40d7e6
    Author: Keyrxng <106303466+Keyrxng@users.noreply.github.com>
    Date:   Fri Sep 1 20:19:41 2023 +0100
    
        fix: add UUPS
    
        added UUPS to Counter, including test and deployment script

---
+++ dapp/contracts/script/CounterDeploy.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "forge-std/Test.sol";
+import "../src/Counter.sol";
+import "../src/uupsProxy.sol";
+
+
+contract CounterDeployScript is Script {
+    Counter counter;
+    Counter counterCast;
+    UupsProxy proxy;
+    
+    function run() external {
+        vm.startBroadcast();
+
+        bytes memory payload = abi.encodeWithSignature("initialize(uint256)", 0.5);
+        counter = new Counter();
+        console.log("Counter address: %s", address(counter));
+        proxy = new UupsProxy(address(counter), payload);
+        console.log("Proxy address: %s", address(proxy));
+        counterCast = Counter(address(proxy));
+        console.log("Counter cast address: %s", address(counterCast));
+        integrationTest();
+        vm.stopBroadcast();
+    }
+
+    function integrationTest() internal view {
+        uint expected = 5;
+        uint actual = counterCast.number();
+        require(actual == expected, "Counter should be initialized to 5");  
+        console.log("Counter initialized to %s", actual);
+        console.log("Successfully deployed and initialized Counter contract");
+    }
+}
--- dapp/contracts/src/CounterOneChange.sol
+++ dapp/contracts/src/CounterOneChange.sol
@@ -1,14 +1,17 @@
-import { UUPSUpgradeable } from "openzeppelinUpgradeable/proxy/utils/UUPSUpgradeable.sol";
-import { Initializable } from "openzeppelinUpgradeable/proxy/utils/Initializable.sol";
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-contract Counter is Initializable, UUPSUpgradeable {
+import {UUPSUpgradeable} from "openzeppelinUpgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "openzeppelinUpgradeable/proxy/utils/Initializable.sol";
+
+contract CounterOneChange is Initializable, UUPSUpgradeable {
     uint256 public number;
 
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(uint256 num) internal initializer {
+    function initialize(uint num) public initializer {
         __UUPSUpgradeable_init();
         number = num;
     }
@@ -25,10 +28,14 @@ contract Counter is Initializable, UUPSUpgradeable {
         return __gap.length;
     }
 
+    function decrement() public {
+        number--;
+    }
+
     function _authorizeUpgrade(address newImplementation)
         internal
         override
     {}
 
-    uint256[50] private __gap;
+    uint256[49] private __gap;
 }
